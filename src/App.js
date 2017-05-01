4caa7a4da0581462bba9333b27d0e345e35da099
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-05-01 17:44:35 -0600

    formatted

--- src/App.js
+++ src/App.js
@@ -1,90 +1,82 @@
 let genericRecipes = [
-    {
-        picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
-        title: 'Fancy Cake',
-        ingredients: [
-          {
-            name: 'sugar',
-            amount: '2 oz.'
-          },
-          {
-            name: 'flour',
-            amount: '1 cup'
-          }
-        ],
-        directions: 'mix and bake'
-    },
-    {
-        picture: '',
-        title: 'Banana Pudding',
-        ingredients: [
-          {
-            name: 'bananas',
-            amount: '2'
-          },
-          {
-            name: 'vanilla wafers',
-            amount: '1/2 cup'
-          },
-          {
-            name: 'banana pudding',
-            amount: '1 package'
-          }
-        ],
-        directions: 'Cook banana pudding. Mix in vanilla wafers and sliced bananas. Simmer for 20 minutes.'
-    },
-    {
-        picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
-        title: 'Fancy Cake',
-        ingredients: [
-          {
-            name: 'sugar',
-            amount: '2 oz.'
-          },
-          {
-            name: 'flour',
-            amount: '1 cup'
-          }
-        ],
-        directions: 'Lorem ipsum dolor sit amet, ut nec quando legendos accusata, modus sensibus usu ea, ad ipsum velit qualisque nec. Pertinax persecuti ut mei. Vis ei eius veri verterem, eu illum commodo copiosae mei. Et eum tollit quodsi, pro aperiri contentiones ex. '
-    },
-    {
-        picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
-        title: 'Fancy Cake',
-        ingredients: [
-          {
-            name: 'sugar',
-            amount: '2 oz.'
-          },
-          {
-            name: 'flour',
-            amount: '1 cup'
-          }
-        ],
-        directions: 'mix and bake'
-    },
-    {
-        picture: '',
-        title: 'Jello',
-        ingredients: [
-          {
-            name: 'bananas',
-            amount: '2'
-          },
-          {
-            name: 'vanilla wafers',
-            amount: '1/2 cup'
-          },
-          {
-            name: 'banana pudding',
-            amount: '1 package'
-          }
-        ],
-        directions: 'Cook banana pudding. Mix in vanilla wafers and sliced bananas. Simmer for 20 minutes.'
-    },
+  {
+    picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
+    title: 'Fancy Cake',
+    ingredients: [
+      {
+        name: 'sugar',
+        amount: '2 oz.'
+      }, {
+        name: 'flour',
+        amount: '1 cup'
+      }
+    ],
+    directions: 'mix and bake'
+  }, {
+    picture: '',
+    title: 'Banana Pudding',
+    ingredients: [
+      {
+        name: 'bananas',
+        amount: '2'
+      }, {
+        name: 'vanilla wafers',
+        amount: '1/2 cup'
+      }, {
+        name: 'banana pudding',
+        amount: '1 package'
+      }
+    ],
+    directions: 'Cook banana pudding. Mix in vanilla wafers and sliced bananas. Simmer for 20 min' +
+        'utes.'
+  }, {
+    picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
+    title: 'Fancy Cake',
+    ingredients: [
+      {
+        name: 'sugar',
+        amount: '2 oz.'
+      }, {
+        name: 'flour',
+        amount: '1 cup'
+      }
+    ],
+    directions: 'Lorem ipsum dolor sit amet, ut nec quando legendos accusata, modus sensibus usu ' +
+        'ea, ad ipsum velit qualisque nec. Pertinax persecuti ut mei. Vis ei eius veri ve' +
+        'rterem, eu illum commodo copiosae mei. Et eum tollit quodsi, pro aperiri content' +
+        'iones ex. '
+  }, {
+    picture: 'http://satinflame.com/wp-content/uploads/2017/03/IMG_5708-1024x683.jpg',
+    title: 'Fancy Cake',
+    ingredients: [
+      {
+        name: 'sugar',
+        amount: '2 oz.'
+      }, {
+        name: 'flour',
+        amount: '1 cup'
+      }
+    ],
+    directions: 'mix and bake'
+  }, {
+    picture: '',
+    title: 'Jello',
+    ingredients: [
+      {
+        name: 'bananas',
+        amount: '2'
+      }, {
+        name: 'vanilla wafers',
+        amount: '1/2 cup'
+      }, {
+        name: 'banana pudding',
+        amount: '1 package'
+      }
+    ],
+    directions: 'Cook banana pudding. Mix in vanilla wafers and sliced bananas. Simmer for 20 min' +
+        'utes.'
+  }
 ];
-
-
 
 import React, {Component} from 'react';
 import './App.css';
@@ -92,36 +84,31 @@ import AddRecipe from './AddRecipe';
 import DisplayRecipe from './DisplayRecipe';
 import Header from './Header';
 import Jumbotron from './Jumbotron';
+//import PropTypes from 'prop-types';
 
 export default class App extends Component {
 
- constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      
       recipelist: []
-      //recipelist: recipelist.splice(2,1)
     }
-}
-
- 
+  }
 
   componentDidMount() {
-     var localRecipes =  localStorage.getItem("_jayv30_recipes");
-  let recipelist2;
+    var localRecipes = localStorage.getItem("_jayv30_recipes");
+    let recipes;
 
     //console.log("recipes from localStorage: ", localRecipes);
     if (!localRecipes) {
-      recipelist2 = JSON.stringify(genericRecipes);
+      recipes = JSON.stringify(genericRecipes);
     } else {
-      recipelist2 = localRecipes;
+      recipes = localRecipes;
     }
-    this.setState({      
-      recipelist: JSON.parse(recipelist2)
+    this.setState({
+      recipelist: JSON.parse(recipes)
     });
-
-    console.log(this.state.recipelist);
   }
 
   render() {
@@ -138,18 +125,17 @@ export default class App extends Component {
           <div className='container'>
             <div className="row">
               <div className="card-columns">
-              { recipelist.map((item, index) => {
-                    return <DisplayRecipe 
+                {recipelist.map((item, index) => {
+                  return <DisplayRecipe
                     key={index}
                     elementNo={index}
                     title={item.title}
-                    picture={item.picture}                        
+                    picture={item.picture}
                     directions={item.directions}
-                    ingredients={item.ingredients}
-                    />
+                    ingredients={item.ingredients}/>
                 })}
 
-              <AddRecipe/>
+                <AddRecipe/>
               </div>
             </div>
           </div>
